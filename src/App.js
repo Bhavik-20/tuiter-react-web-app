480d9fc9e0e90bf161776bd31b719b9ec49bb900
Bhavik-20 <bbhatt2005@gmail.com>
2023-07-23 09:48:20 -0400

    A3 final exercise error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Labs from "./labs";
 import HelloWorld from "./labs/a3/hello-world";
 import Tuiter from "./tuiter";
-import {HashRouter} from "react-router-dom";
-import {Routes, Route} from "react-router";
+import { HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router";
+import { Navigate } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
       <div className="container">
-        <Routes>
-          <Route path="/labs/*"   element={<Labs/>}/>
-          <Route path="/hello"    element={<HelloWorld/>}/>
-          <Route path="/tuiter/*" element={<Tuiter/>}/>
-        </Routes>
+      <Routes>
+      <Route path="/"         element={<Navigate to="/labs"/>}/>
+      <Route path="/labs/*"   element={<Labs/>}/>
+      <Route path="/hello"    element={<HelloWorld/>}/>
+      <Route path="/tuiter/*" element={<Tuiter/>}/>
+    </Routes>
       </div>
     </HashRouter>
   );
--- src/labs/a3/index.js
+++ src/labs/a3/index.js
@@ -1,9 +1,19 @@
 import JavaScript from "./java-script";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
+import TodoItem from "./todo/todo-item";
 
 function Assignment3() {
    return(
       <div>
          <h1>Assignment 3</h1>
+         <TodoItem/>
+         <TodoList/>
+         <ConditionalOutput/>
+         <Styles/>
+         <Classes/>
          <JavaScript/>
       </div>
    );
